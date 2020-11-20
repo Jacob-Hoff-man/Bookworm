2aa2ddbe7317a39fbddc349af429bba5cba62a96
Jacob-Hoff-man <cubhoffman@gmail.com>
2020-11-19 21:38:06 -0500

    Version 1.0

---
+++ Bookworm/ContentView.swift
@@ -0,0 +1,75 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Jacob on 10/30/20.
+//  Copyright © 2020 Jacob. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(entity: Book.entity(), sortDescriptors: [
+        NSSortDescriptor(keyPath: \Book.title, ascending: true),
+        NSSortDescriptor(keyPath: \Book.author, ascending: true)
+    ]) var books: FetchedResults<Book>
+    
+    @State private var showingAddScreen = false
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(books, id: \.self) { book in
+                    NavigationLink(destination: DetailView(book: book)) {
+                        EmojiRatingView(rating: book.rating)
+                            .font(.largeTitle)
+                        
+                        VStack(alignment: .leading) {
+                            Text(book.title ?? "Unknown Title")
+                                .font(.headline)
+                            Text(book.author ?? "Unknown Author")
+                                .foregroundColor(.secondary)
+                        }
+                        
+                    }
+                }
+                .onDelete(perform: deleteBooks)
+            }
+            .navigationBarTitle("Bookworm")
+            .navigationBarItems(leading: EditButton(),
+                                trailing: Button(action: {
+                                    self.showingAddScreen.toggle()
+                                }) {
+                                    Image(systemName: "plus")
+                }
+            )
+                .sheet(isPresented: $showingAddScreen) {
+                    AddBookView().environment(\.managedObjectContext, self.moc)
+            }
+        }
+        
+    }
+    
+    func deleteBooks(at offsets: IndexSet) {
+        for offset in offsets {
+            //find this book in the fetch request
+            let book = books[offset]
+            
+            //delete the book from the context
+            moc.delete(book)
+            
+            //save context
+            try? moc.save()
+        }
+    }
+    
+}
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
